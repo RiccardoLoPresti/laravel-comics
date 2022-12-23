15ea9325f707034a0a526f3cd3b39b8e23017e0f
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-12-23 16:47:29 +0100

    fix cta

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -25,6 +25,33 @@
             <button>Load More</button>
         </div>
     </div>
+
+
 </main>
+
+<section>
+    <div class="container container-cta">
+
+        <nav>
+            <ul>
+                @foreach ($elements as $element )
+
+                    <li>
+                        <a href="{{$element['href']}}">
+                            <div class="image">
+                                <img src="{{$element['img']}}" alt="{{$element['text']}}">
+                            </div>
+                            <div class="text">
+                                <span>{{$element['text']}}</span>
+                            </div>
+                        </a>
+                    </li>
+
+                @endforeach
+            </ul>
+        </nav>
+
+    </div>
+</section>
 
 @endsection
